21ad5bf13e8cd9e1c473f6cee8b31a413c563ee8
BlackBox <1647010108@qq.com>
2022-05-31 15:13:39 +0800

    Initial commit

---
+++ api/src/test/java/org/blackbox/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.blackbox.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
